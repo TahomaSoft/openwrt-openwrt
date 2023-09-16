7b06e863de0a2ad0199624123664283d43b37d42
TahomaSoft <bacon@tahomasoft.com>
2023-09-16 18:24:38 -0400

    starter

---
+++ target/linux/rockchip/rk3568/target.mk
@@ -0,0 +1,8 @@
+ARCH:=aarch64
+SUBTARGET:=rk3568
+BOARDNAME:=RK3568 boards (64 bit)
+
+define Target/Description
+	Build firmware image for Rockchip RK35368 devices.
+	This firmware features a 64 bit kernel.
+endef
