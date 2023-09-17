69f95603f5a9cfed3668e0a374169a22e2d4c2e6
TahomaSoft <bacon@tahomasoft.com>
2023-09-17 19:16:59 -0400

    updated target with loder offset

--- target/linux/rockchip/image/rk3568.mk
+++ target/linux/rockchip/image/rk3568.mk
@@ -1,6 +1,14 @@
 # SPDX-License-Identifier: GPL-2.0-only
 #
 # Copyright (C) 2020 Tobias Maedel
+# Modified for Rk3568 rockchip family by Tahomasoft. 17 Sept 2023
+
+
+# FIT will be loaded at 0x02080000. Leave 16M for that, align it to 2M and load the kernel after it.
+
+
+KERNEL_LOADADDR := 0x03200000
+
 
 define Device/linkstar_h68k
   DEVICE_VENDOR := Seeed
