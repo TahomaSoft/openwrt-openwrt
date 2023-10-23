4b14527f5fe679ad203385dc7ae0da73a68b1c49
TahomaSoft <bacon@tahomasoft.com>
2023-10-22 22:29:26 -0400

    aligning names on makefiles for linux and uboot

--- target/linux/rockchip/image/rk3568.mk
+++ target/linux/rockchip/image/rk3568.mk
@@ -13,8 +13,8 @@ KERNEL_LOADADDR := 0x03200000
 define Device/linkstar_h68k
   DEVICE_VENDOR := Seeed
   SOC := rk3568
-  DEVICE_MODEL := linkstar_h68k
-  UBOOT_DEVICE_NAME := linkstar_h68k
+  DEVICE_MODEL := LinkstarH68k
+  UBOOT_DEVICE_NAME := linkstar-h68k-rk3568
   IMAGE/sysupgrade.img.gz := boot-common | boot-script linkstar | linkstar-img | gzip | append-metadata
   DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-mt7921e kmod-r8125 kmod-usb-serial-cp210x wpad-openssl ethtool
 endef
