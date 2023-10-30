df6e6cbc8eacae5f1d4067f50893d58136c37f38
TahomaSoft <bacon@tahomasoft.com>
2023-10-29 22:17:49 -0400

    wondering around the make files

--- target/linux/rockchip/rk3568/target.mk
+++ target/linux/rockchip/rk3568/target.mk
@@ -1,6 +1,13 @@
-ARCH:=aarch64
-SUBTARGET:=rk3568
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2023 Erik H. Beck bacon@tahomasoft.com
+
+include $(TOPDIR)/rules.mk
+
 BOARDNAME:=RK3568 boards (64 bit)
+CPU_TYPE:=armv8
+CPU_SUBTYPE:=rk3568
+
 
 define Target/Description
 	Build firmware image for Rockchip RK35368 devices.
