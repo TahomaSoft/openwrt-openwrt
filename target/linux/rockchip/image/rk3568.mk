66d5c85c946bcbf195ce3ca51b348a9bc8af8953
TahomaSoft <bacon@tahomasoft.com>
2023-11-10 16:57:57 -0500

    make file fixes uboot and rk3568-linux. disable binman in u-boot

--- target/linux/rockchip/image/rk3568.mk
+++ target/linux/rockchip/image/rk3568.mk
@@ -7,16 +7,17 @@
 # FIT will be loaded at 0x02080000. Leave 16M for that, align it to 2M and load the kernel after it.
 
 
-KERNEL_LOADADDR := 0x03200000
+KERNEL_LOADADDR:= 0x03200000
 
 
 define Device/linkstar_h68k
-	DEVICE_VENDOR := Seeed
-	SOC := rk3568
-	DEVICE_MODEL := LinkstarH68k
-	UBOOT_DEVICE_NAME := linkstar-h68k-rk3568
+	DEVICE_VENDOR:= Seeed
+	SOC:= rk3568
+	DEVICE_MODEL:= LinkstarH68k
+	UBOOT_DEVICE_NAME:= linkstar-h68k-rk3568
+#	DEPENDS:=+u-boot-linkstar-h68k
 	IMAGE/sysupgrade.img.gz := boot-common | boot-script linkstar | rk3568-img | gzip | append-metadata
-	DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-mt7921e kmod-r8125 kmod-usb-serial-cp210x wpad-openssl ethtool
+	DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-mt7921e kmod-r8125 kmod-usb-serial-cp210x wpad-openssl ethtool u-boot-linkstar-h68k
 
 # removing kmod-mt7921e for debugging purposes; takes a long time to compile this module
 #	DEVICE_PACKAGES := kmod-ata-ahci-platform  kmod-r8125 kmod-usb-serial-cp210x wpad-openssl ethtool
