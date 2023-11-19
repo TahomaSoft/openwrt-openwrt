9e034e04ea91d440aaf57505835debd754c258c9
TahomaSoft <bacon@tahomasoft.com>
2023-11-19 11:26:03 -0500

    starting paraigm shift in image build

--- target/linux/rockchip/image/rk3568.mk
+++ target/linux/rockchip/image/rk3568.mk
@@ -1,13 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 #
-# Copyright (C) 2020 Tobias Maedel
 # Modified for Rk3568 rockchip family by Tahomasoft. 17 Sept 2023
-
-
-# FIT will be loaded at 0x02080000. Leave 16M for that, align it to 2M and load the kernel after it.
-
-
-KERNEL_LOADADDR:= 0x03200000
+# Restart november 2023
 
 
 define Device/linkstar_h68k
@@ -15,12 +9,6 @@ define Device/linkstar_h68k
 	SOC:= rk3568
 	DEVICE_MODEL:= LinkstarH68k
 	UBOOT_DEVICE_NAME:= linkstar-h68k-rk3568
-#	DEPENDS:=+u-boot-linkstar-h68k
-	IMAGE/sysupgrade.img.gz := boot-common | boot-script linkstar | rk3568-img | gzip | append-metadata
-	DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-mt7921e kmod-r8125 kmod-usb-serial-cp210x wpad-openssl ethtool u-boot-linkstar-h68k rk3568-boot-meta
-
-# removing kmod-mt7921e for debugging purposes; takes a long time to compile this module
-#	DEVICE_PACKAGES := kmod-ata-ahci-platform  kmod-r8125 kmod-usb-serial-cp210x wpad-openssl ethtool
 endef
 
 TARGET_DEVICES += linkstar_h68k
